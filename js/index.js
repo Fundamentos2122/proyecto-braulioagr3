05575cc18897bc645593c599bd4f09c26e65908b
braulioagr3 <31120570+braulioagr3@users.noreply.github.com>
2021-11-06 20:41:23 -0600

    Preparativos

---
+++ js/index.js
@@ -0,0 +1,83 @@
+function documentLoad(){
+    loadMainMenu();
+    loadMenu();
+    addClickCards();
+}
+
+function loadMainMenu(){
+    var encabezado;
+    encabezado = document.getElementById('Header');
+    encabezado.innerHTML = `<nav class="navbar">   
+                                <div class="navbar-collapse">
+                                    <ul class="navbar-nav">
+                                        <li class="nav-item">
+                                            <a href="index.html" class="nav-link">
+                                                    <img src="images/logo.png" alt="Logo">
+                                            </a>
+                                        </li>
+                                        <li class="nav-item">
+                                            <a href="" class="nav-link h4">
+                                                Barra de busqueda
+                                            </a>
+                                        </li>
+                                        <li class="nav-item">
+                                            <a href="" class="nav-link h4">Carrito</a>
+                                        </li>
+                                        <li class="nav-item">
+                                            <a href="login.html" class="nav-link h4">Iniciar Sesión</a>
+                                        </li>
+                                        <li class="nav-item">
+                                            <a href="singup.html" class="nav-link h4">Registro</a>
+                                        </li>
+                                    </ul>
+                                </div>
+                            </nav>`;
+}
+
+
+function loadMenu(){
+    var menu;
+    menu = document.getElementById('Menu');
+    menu.innerHTML =    `<nav class="navbar">   
+                            <div class="navbar-collapse">
+                                <ul class="navbar-nav">
+                                    <li class="nav-item nav-button">
+                                        <button onclick="menuAction()">-</button>
+                                    </li>   
+                                    <li class="nav-item">
+                                        <a href="autores.html" class="nav-link h4 hidden">Autores</a>
+                                    </li>
+                                    <li class="nav-item">
+                                        <a href="generos.html" class="nav-link h4 hidden">Generos</a>
+                                    </li>
+                                    <li class="nav-item">
+                                        <a href="series.html" class="nav-link h4 hidden">Series</a>
+                                    </li>
+                                    <li class="nav-item">
+                                        <a href="editoriales.html" class="nav-link h4 hidden">Editoriales</a>
+                                    </li>                     
+                                </ul>
+                            </div>
+                        </nav>`;
+}
+       
+function addClickCards()
+{
+    var cards= document.getElementsByClassName('card');
+    for(var i = 0; i < cards.length; i++){
+        cards[i].addEventListener('click', function (event) {
+            window.location.href = "details.html";
+        });}
+}
+function addCar(){
+    alert("Su articulo se añadio al carrito");
+    window.location.href = "shoppingCart.html";
+}
+function menuAction(){
+    var menu;
+    menu = document.getElementById('Menu');
+    var ancla = menu.getElementsByClassName('nav-link');
+    for(var i = 0; i < ancla.length; i++){
+        ancla[i].classList.toggle('hidden');
+    }
+}
